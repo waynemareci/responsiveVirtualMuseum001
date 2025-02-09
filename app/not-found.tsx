4b353e9fe8f6820f36080d3339b0ff2b0c99e35d
Wayne Mareci <Wayne@Mareci.com>
2025-02-09 15:45:15 -0500

    not found page responsive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,27 @@
-import Link from 'next/link'
+import Link from "next/link";
 import React from "react";
- 
+import "./styles/notFound.css"
+
 export default function NotFound() {
   return (
-    <div style={{ color: 'white', backgroundImage: `url('/virtualMuseum404.png')`, backgroundSize: 'cover', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <h2 className="mt-5" >Not Found</h2>
-      <p>something went wrong</p>
-      <Link style={{color: 'white'}} href="/">return to search page</Link>
+    <div
+      style={{
+        color: "white",
+        backgroundSize: "cover",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="notFoundText">
+        <h2>Not Found</h2>
+        <p>something went wrong</p>
+        <Link style={{ color: "white" }} href="/">
+          return to search page
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
